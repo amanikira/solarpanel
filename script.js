b77e2173428bd5e26c40c201afc7d62a3c84569a
amanikira <amanikira2@gmail.com>
2024-09-24 17:47:42 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Calculator Script
+function calculateSavings() {
+    const usage = document.getElementById("usage").value;
+    const rate = document.getElementById("rate").value;
+    const sunlight = document.getElementById("sunlight").value;
+
+    if (usage && rate && sunlight) {
+        const monthlyCost = usage * rate;
+       
